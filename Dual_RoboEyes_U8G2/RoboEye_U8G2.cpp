19c19d2e0d73e1921228a752afe8abe4d523b25a
lIMpY <xxdilbertxx@aim.com>
2025-05-08 20:47:02 -0400

    wtf

--- Dual_RoboEyes_U8G2/RoboEye_U8G2.cpp
+++ Dual_RoboEyes_U8G2/RoboEye_U8G2.cpp
@@ -1,52 +1,117 @@
-#ifndef ROBO_EYE_U8G2_H
-#define ROBO_EYE_U8G2_H
-
-#include <Arduino.h>
-#include <U8g2lib.h>
-
-class RoboEye_U8G2 {
-public:
-  RoboEye_U8G2(U8G2 &display, bool isRight);
-
-  void begin();
-  void setTarget(float normX, float normY);
-  void update();
-  
-private:
-  U8G2 &u8g2;
-  bool isRightEye;
-
-  int eyeCenterX = 64;
-  int eyeCenterY = 32;
-  int eyeWidth = 20;
-  int eyeHeight = 10; 
-  int pupilWidth = 4;
-  int pupilHeight = 8;
-
-  float targetX = 0;
-  float targetY = 0;
-  float currentX = 0;
-  float currentY = 0;
-  float eyeFollowX = 0;
-  float eyeFollowY = 0;
-  float lookX = 0;
-  float lookY = 0;
-  float eyeVelX = 0;
-  float eyeVelY = 0;
+#include "RoboEye_U8G2.h"
 
 
+RoboEye_U8G2::RoboEye_U8G2(U8G2 &display, bool isRight)
+  : u8g2(display), isRightEye(isRight) {}
 
-  unsigned long lastBlink = 0;
-  bool blinking = false;
-  int blinkFrame = 0;
 
-  bool mad = false;
+void RoboEye_U8G2::begin() {
+  u8g2.begin();
+}
 
-  void updateBlink();
-  void drawEye();
-  void drawPupil();
-  void drawLids();
 
-};
+void RoboEye_U8G2::setTarget(float normX, float normY) {
+  normX = constrain(normX, -1.0, 1.0);
+  normY = constrain(normY, -1.0, 1.0);
 
-#endif
+  // Move the full eye within screen bounds
+  targetX = normX * (64 - eyeWidth);
+  targetY = normY * (32 - eyeHeight);
+}
+
+
+void RoboEye_U8G2::update() {
+  // Smoothly move pupil toward the full target
+  currentX += (targetX - currentX) * 0.5;
+  currentY += (targetY - currentY) * 0.5;
+
+  // Let the eye white follow the pupil directly, but with spring dynamics
+  const float stiffness = 0.1;
+  const float damping   = 0.75;
+
+  float springX = currentX - eyeFollowX;
+  float springY = currentY - eyeFollowY;
+
+  eyeVelX = eyeVelX * damping + springX * stiffness;
+  eyeVelY = eyeVelY * damping + springY * stiffness;
+
+  eyeFollowX += eyeVelX;
+  eyeFollowY += eyeVelY;
+
+  // Calculate where the eye white will be on screen
+  lookX = 64 + eyeFollowX;
+  lookY = 32 + eyeFollowY;
+
+  // Clamp to screen bounds
+  lookX = constrain(lookX, eyeWidth / 2, 128 - eyeWidth / 2);
+  lookY = constrain(lookY, eyeHeight / 2, 64 - eyeHeight / 2);
+
+  updateBlink();
+  drawEye();
+}
+
+
+void RoboEye_U8G2::updateBlink() {
+  unsigned long now = millis();
+
+  if (!blinking && now - lastBlink > 8000) {
+    blinking = true;
+    blinkFrame = 0;
+    lastBlink = now;
+  }
+
+  if (blinking) {
+    blinkFrame++;
+    if (blinkFrame >= 10) {
+      blinking = false;
+    }
+  }
+}
+
+
+void RoboEye_U8G2::drawEye() {
+  u8g2.clearBuffer();
+  u8g2.setDrawColor(1);
+  u8g2.drawFilledEllipse(lookX, lookY, eyeWidth, eyeHeight);
+  u8g2.setDrawColor(0);
+  if (mad) {
+    if (isRightEye) u8g2.drawTriangle(lookX - eyeWidth, lookY + eyeHeight, lookX + eyeWidth, lookY - eyeHeight,0,0);
+    else u8g2.drawTriangle(lookX + eyeWidth, lookY + eyeHeight, lookX - eyeWidth, lookY - eyeHeight, 128, 0);
+  }
+  drawPupil();
+  drawLids();
+  u8g2.sendBuffer();
+}
+
+
+void RoboEye_U8G2::drawPupil() {
+  u8g2.setDrawColor(0);
+
+  // Calculate constrained offsets relative to the center of the eye
+  float maxOffsetX = eyeWidth - pupilWidth;
+  float maxOffsetY = eyeHeight - pupilHeight;
+
+  float pupilOffsetX = constrain(currentX, -maxOffsetX, maxOffsetX);
+  float pupilOffsetY = constrain(currentY, -maxOffsetY, maxOffsetY);
+
+  u8g2.drawFilledEllipse(lookX + pupilOffsetX, lookY + pupilOffsetY, pupilWidth, pupilHeight);
+}
+
+
+void RoboEye_U8G2::drawLids() {
+  if (!blinking) return;
+
+  u8g2.setDrawColor(0);
+  int lidAmount = (blinkFrame <= 5) ? blinkFrame : (10 - blinkFrame);
+  lidAmount = map(lidAmount, 0, 5, 0, eyeHeight + pupilHeight);
+
+  int topY = lookY - lidAmount;
+  int bottomY = lookY + lidAmount;
+
+  // Clip so we don't draw outside the screen
+  topY = max(0, topY);
+  bottomY = min(64, bottomY);
+
+  u8g2.drawBox(0, 0, 128, topY);               // Top lid
+  u8g2.drawBox(0, bottomY, 128, 64 - bottomY); // Bottom lid
+}
